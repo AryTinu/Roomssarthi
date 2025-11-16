5b3624fbb4d2913167c084abc635b7d703e8b15e
AryTinu <Shendearyan966@gmail.com>
2025-11-16 11:05:58 +0530

    api fix

--- roomssarthi/src/pages/Register.js
+++ roomssarthi/src/pages/Register.js
@@ -47,7 +47,7 @@ const Register = () => {
     setErrors({});
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://roomssarthi.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
